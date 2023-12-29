282bd952446833d81cd9d8a6588eeddd99f3b46f
Ever30 <evermz@Eves-MacBook-Air.local>
2023-12-29 13:59:46 -0600

    homework

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "0ea84f30e5a266575099122414a3c0ee"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0ea84f30e5a266575099122414a3c0ee"
