fdac16a5db884276cd90ae7aa8db3470c4877912
Ever30 <149534473+Ever30@users.noreply.github.com>
2023-12-29 14:01:09 -0600

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "0ea84f30e5a266575099122414a3c0ee"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "0ea84f30e5a266575099122414a3c0ee"
+geoapify_key = ""
